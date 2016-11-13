00ab5db65cddbc28f1368dc0943da8a421f80712
iftee <ifteeh01@gmail.com>
2016-11-14 00:13:47 +0600

    first commit

--- web/index.php
+++ web/index.php
@@ -311,8 +311,7 @@ $('#fanback').stop().fadeOut('medium');
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCidXFiuqXTl47kMUQUEEK4EwSCpG8ZBoY&callback=initMap">
     </script>
 
-    <iframe allowtransparency='true' frameborder='0' scrolling='no' src='//www.facebook.com/plugins/likebox.php? href=https://www.facebook.com/ShkarkoMuzikShqip&width=402&height=255&colorscheme=light&show_faces=true&show_border=false&stream=false&header=false' style='border: none; overflow: hidden; margin-top: -19px; width: 402px; height: 230px;'></iframe>
-
+   
     
     <div class="fb-like" data-href="https://www.facebook.com/987448978039436" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
 
